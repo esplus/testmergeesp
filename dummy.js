b57ddf660329503db59a90605f5f81d60f91f10a
murtuza <murtuza.noorani@northgateps.com>
2016-07-01 17:44:04 +0530

    testmergecomit

---
+++ dummy.js
@@ -0,0 +1,269 @@
+// API callback
+SearchCompleted({
+ "kind": "customsearch#search",
+ "url": {
+  "type": "application/json",
+  "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&cref={cref?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+ },
+ "queries": {
+  "nextPage": [
+   {
+    "title": "Google Custom Search - end",
+    "totalResults": "233000",
+    "searchTerms": "end",
+    "count": 10,
+    "startIndex": 11,
+    "inputEncoding": "utf8",
+    "outputEncoding": "utf8",
+    "safe": "off",
+    "cx": "017754066774365358043:lyjyorqiptq"
+   }
+  ],
+  "request": [
+   {
+    "title": "Google Custom Search - end",
+    "totalResults": "233000",
+    "searchTerms": "end",
+    "count": 10,
+    "startIndex": 1,
+    "inputEncoding": "utf8",
+    "outputEncoding": "utf8",
+    "safe": "off",
+    "cx": "017754066774365358043:lyjyorqiptq"
+   }
+  ]
+ },
+ "context": {
+  "title": "Theiet.org"
+ },
+ "searchInformation": {
+  "searchTime": 0.260024,
+  "formattedSearchTime": "0.26",
+  "totalResults": "233000",
+  "formattedTotalResults": "233,000"
+ },
+ "items": [
+  {
+   "kind": "customsearch#result",
+   "title": "Light at the end of the pipe - The IET",
+   "htmlTitle": "Light at the \u003cb\u003eend\u003c/b\u003e of the pipe - The IET",
+   "link": "http://www.theiet.org/events/local/203894.cfm",
+   "displayLink": "www.theiet.org",
+   "snippet": "Light at the end of the pipe. Lecture. Image of three blue heads in profile on grey \nbackground. Light at the core of our telecoms networks. Register ...",
+   "htmlSnippet": "Light at the \u003cb\u003eend\u003c/b\u003e of the pipe. Lecture. Image of three blue heads in profile on grey \u003cbr\u003e\nbackground. Light at the core of our telecoms networks. Register&nbsp;...",
+   "cacheId": "61vK2FyaCEgJ",
+   "formattedUrl": "www.theiet.org/events/local/203894.cfm",
+   "htmlFormattedUrl": "www.theiet.org/events/local/203894.cfm",
+   "pagemap": {
+    "postaladdress": [
+     {
+      "addresslocality": "High Wycombe",
+      "addresscountry": "United Kingdom",
+      "url": "High Wycombe - Bucks New University"
+     },
+     {
+      "addresslocality": "London"
+     }
+    ],
+    "hcalendar": [
+     {
+      "dtstart": "2015-02-19",
+      "summary": "Light at the end of the pipe",
+      "dtend": "2015-02-19",
+      "description": "Light at the core of our telecoms networks",
+      "photo": "http://www.theiet.org/events/externalwebtest/icon-ln-tpn.jpg"
+     }
+    ],
+    "event": [
+     {
+      "dtstart": "2015-02-19",
+      "summary": "Light at the end of the pipe",
+      "dtend": "2015-02-19"
+     },
+     {
+      "name": "Light at the end of the pipe",
+      "image": "http://www.theiet.org/events/externalwebtest/icon-ln-tpn.jpg",
+      "startdate": "2015-02-19T19:30:00Z+00:00",
+      "enddate": "2015-02-19T21:00:00Z+00:00",
+      "description": "Light at the core of our telecoms networks"
+     },
+     {
+      "url": "Appleton Lecture - Cyber security",
+      "name": "Appleton Lecture - Cyber security",
+      "image": "http://www.theiet.org/events/calendar/event-image.jpg",
+      "startdate": "2015-01-22T18:00:00Z+00:00",
+      "enddate": "2015-01-22T21:00:00Z+00:00",
+      "description": "Join us on 22 January 2015 for this free-to-attend evening to hear James Quinault CBE present on \"Cyber security: why we should be worried; and what we can do about it\"."
+     }
+    ],
+    "metatags": [
+     {
+      "contentid": "203894",
+      "resource type": "Events",
+      "date": "1 October 2014",
+      "dcsext.taxonomyid": "119;146",
+      "dcsext.taxonomytext": "Information and Communications;Telecommunications"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "End to lens envy?",
+   "htmlTitle": "\u003cb\u003eEnd\u003c/b\u003e to lens envy?",
+   "link": "http://www.theiet.org/forums/blog/blogpost.cfm?threadid=57101&catid=405",
+   "displayLink": "www.theiet.org",
+   "snippet": "Mar 24, 2014 ... Nothing could be more emasculating than to whip out your feeble instrument next \nto guy with a massive, well-built body - complete with huge ...",
+   "htmlSnippet": "Mar 24, 2014 \u003cb\u003e...\u003c/b\u003e Nothing could be more emasculating than to whip out your feeble instrument next \u003cbr\u003e\nto guy with a massive, well-built body - complete with huge&nbsp;...",
+   "cacheId": "6TANEbHLwKMJ",
+   "formattedUrl": "www.theiet.org/forums/blog/blogpost.cfm?threadid=57101...",
+   "htmlFormattedUrl": "www.theiet.org/forums/blog/blogpost.cfm?threadid=57101...",
+   "pagemap": {
+    "metatags": [
+     {
+      "resource type": "Blog"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "Light at the end of the tunnel - Electronics Letters - The IET",
+   "htmlTitle": "Light at the \u003cb\u003eend\u003c/b\u003e of the tunnel - Electronics Letters - The IET",
+   "link": "http://www.theiet.org/resources/journals/eletters/5009/light-end-tunnel.cfm",
+   "displayLink": "www.theiet.org",
+   "snippet": "Apr 29, 2014 ... In efforts toward creating practical chip-level optical interconnects, researchers in \nCanada present the first nanoscale tube based photodetector, ...",
+   "htmlSnippet": "Apr 29, 2014 \u003cb\u003e...\u003c/b\u003e In efforts toward creating practical chip-level optical interconnects, researchers in \u003cbr\u003e\nCanada present the first nanoscale tube based photodetector,&nbsp;...",
+   "cacheId": "c87HHcXPVDEJ",
+   "formattedUrl": "www.theiet.org/resources/journals/eletters/.../light-end-tunnel.cfm",
+   "htmlFormattedUrl": "www.theiet.org/resources/journals/eletters/.../light-\u003cb\u003eend\u003c/b\u003e-tunnel.cfm",
+   "pagemap": {
+    "cse_image": [
+     {
+      "src": "http://www.theiet.org/resources/journals/eletters/5009/images/640_photodetecor-640x426.jpg"
+     }
+    ],
+    "cse_thumbnail": [
+     {
+      "width": "275",
+      "height": "183",
+      "src": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTEtXEdHaKGEk0q-re-kFVjbX3tcVjbOeJm9xgE8rrFBZYPDqjzPyjSNusz"
+     }
+    ],
+    "metatags": [
+     {
+      "contentid": "200023",
+      "resource type": "Article",
+      "date": "28 October 2014"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IET Forums - Low ring main cpc end to end resistance",
+   "htmlTitle": "IET Forums - Low ring main cpc \u003cb\u003eend\u003c/b\u003e to \u003cb\u003eend\u003c/b\u003e resistance",
+   "link": "http://www.theiet.org/forums/forum/messageview.cfm?catid=205&threadid=37378",
+   "displayLink": "www.theiet.org",
+   "snippet": "Hi all, carrying out a periodic today and got readings of .6 for L-L and N-N but a \nreading of .51 on cpc-cpc. Cable is 2.5/1.5 T&E so obviously the ...",
+   "htmlSnippet": "Hi all, carrying out a periodic today and got readings of .6 for L-L and N-N but a \u003cbr\u003e\nreading of .51 on cpc-cpc. Cable is 2.5/1.5 T&amp;E so obviously the&nbsp;...",
+   "cacheId": "yW_zBWAECoMJ",
+   "formattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205...",
+   "htmlFormattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205..."
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IAESTE: A journey without an end - The IET",
+   "htmlTitle": "IAESTE: A journey without an \u003cb\u003eend\u003c/b\u003e - The IET",
+   "link": "http://www.theiet.org/students/work-careers/work-experience/iaeste.cfm",
+   "displayLink": "www.theiet.org",
+   "snippet": "IAESTE: A journey without an end. IAESTE provides paid STEM internships \nabroad while offering individual support to students as well as a social \nprogramme.",
+   "htmlSnippet": "IAESTE: A journey without an \u003cb\u003eend\u003c/b\u003e. IAESTE provides paid STEM internships \u003cbr\u003e\nabroad while offering individual support to students as well as a social \u003cbr\u003e\nprogramme.",
+   "cacheId": "dfYsAVIu45kJ",
+   "formattedUrl": "www.theiet.org/students/work-careers/work-experience/iaeste.cfm",
+   "htmlFormattedUrl": "www.theiet.org/students/work-careers/work-experience/iaeste.cfm",
+   "pagemap": {
+    "cse_image": [
+     {
+      "src": "http://www.theiet.org/students/work-careers/work-experience/images/iaeste.jpg"
+     }
+    ],
+    "cse_thumbnail": [
+     {
+      "width": "216",
+      "height": "144",
+      "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEkH-CjmKJd9l0Bv1Yxo7ZwBCW-JbOkNx2tGbpgkuF61UJswV-_AAVfom0"
+     }
+    ],
+    "metatags": [
+     {
+      "contentid": "187630",
+      "resource type": "General",
+      "date": "17 March 2014"
+     }
+    ]
+   }
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IET Forums - Flourescent end caps",
+   "htmlTitle": "IET Forums - Flourescent \u003cb\u003eend\u003c/b\u003e caps",
+   "link": "http://www.theiet.org/forums/forum/messageview.cfm?catid=205&threadid=%2057601",
+   "displayLink": "www.theiet.org",
+   "snippet": "Anyone know any makes that will accept insulated and sheathed cable as \nopposed to just skinny singles as is the norm within a fitting. some ...",
+   "htmlSnippet": "Anyone know any makes that will accept insulated and sheathed cable as \u003cbr\u003e\nopposed to just skinny singles as is the norm within a fitting. some&nbsp;...",
+   "cacheId": "SbnD8eealkMJ",
+   "formattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205...",
+   "htmlFormattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205..."
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IET Forums - Young Woman Engineer of the Year award - end the ...",
+   "htmlTitle": "IET Forums - Young Woman Engineer of the Year award - \u003cb\u003eend\u003c/b\u003e the \u003cb\u003e...\u003c/b\u003e",
+   "link": "http://www.theiet.org/forums/forum/messageview.cfm?catid=243&threadid=46834&enterthread=y&STARTPAGE=1",
+   "displayLink": "www.theiet.org",
+   "snippet": "Why is it that I am barred from an IET prize for engineering achievement, purely \nbecause of my Y chromosome? I find this offensive, sexist and ...",
+   "htmlSnippet": "Why is it that I am barred from an IET prize for engineering achievement, purely \u003cbr\u003e\nbecause of my Y chromosome? I find this offensive, sexist and&nbsp;...",
+   "cacheId": "gdQQw-pqtp8J",
+   "formattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid...y...",
+   "htmlFormattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid...y..."
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IET Forums - TT Front end RCD",
+   "htmlTitle": "IET Forums - TT Front \u003cb\u003eend\u003c/b\u003e RCD",
+   "link": "http://www.theiet.org/forums/forum/messageview.cfm?catid=205&threadid=53189",
+   "displayLink": "www.theiet.org",
+   "snippet": "I came across a TT installation yesterday without any front end RCD protection. \nCU is a split load single RCD. Unprotected ccts are 2xlighting ...",
+   "htmlSnippet": "I came across a TT installation yesterday without any front \u003cb\u003eend\u003c/b\u003e RCD protection. \u003cbr\u003e\nCU is a split load single RCD. Unprotected ccts are 2xlighting&nbsp;...",
+   "cacheId": "VyJ7s9VkX7wJ",
+   "formattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205...",
+   "htmlFormattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205..."
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IET Forums - Flourescent end caps",
+   "htmlTitle": "IET Forums - Flourescent \u003cb\u003eend\u003c/b\u003e caps",
+   "link": "http://www.theiet.org/forums/forum/messageview.cfm?catid=205&threadid=%2057601&messid=472957&parentid=472947&FTVAR_FORUMVIEWTMP=Branch",
+   "displayLink": "www.theiet.org",
+   "snippet": "Ah - that's a good thought - I'm sure I've seen some waterproof bi-bin tube \nconnectors for things like vivariums and aquariums... - Andy.",
+   "htmlSnippet": "Ah - that&#39;s a good thought - I&#39;m sure I&#39;ve seen some waterproof bi-bin tube \u003cbr\u003e\nconnectors for things like vivariums and aquariums... - Andy.",
+   "cacheId": "xFKu99MvRvkJ",
+   "formattedUrl": "www.theiet.org/forums/forum/messageview.cfm?...",
+   "htmlFormattedUrl": "www.theiet.org/forums/forum/messageview.cfm?..."
+  },
+  {
+   "kind": "customsearch#result",
+   "title": "IET Forums - Earthing armoured cable armour at one end only.",
+   "htmlTitle": "IET Forums - Earthing armoured cable armour at one \u003cb\u003eend\u003c/b\u003e only.",
+   "link": "http://www.theiet.org/forums/forum/messageview.cfm?catid=205&threadid=45676",
+   "displayLink": "www.theiet.org",
+   "snippet": "For a multicore cable with all the loaded cores inside the same armouring the \neffect of single end bonding on volt drop will be negligible.",
+   "htmlSnippet": "For a multicore cable with all the loaded cores inside the same armouring the \u003cbr\u003e\neffect of single \u003cb\u003eend\u003c/b\u003e bonding on volt drop will be negligible.",
+   "cacheId": "9j3UbLih_X0J",
+   "formattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205...",
+   "htmlFormattedUrl": "www.theiet.org/forums/forum/messageview.cfm?catid=205..."
+  }
+ ]
+}
+);
